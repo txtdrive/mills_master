91359e668c440886d317821f7548fb64936e9bc9
txtdrive <txtdrive@gmail.com>
2021-09-05 12:00:24 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('#fullPage').fullpage({
+    anchors:['s1','s2','s3','s4'],
+    navigation: true,
+    controlArrows:false,
+    slidesNavigation:true,
+    scrollOverflow:true,
+    navigationPosition:true,
+    paddingTop: '8rem',
+    paddingBottom: '8rem',
+    navigationPosition : 'top',
+    slidesNavPosition: 'top'
+})
